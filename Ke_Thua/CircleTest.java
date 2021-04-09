183cf17b6b53ba8636d6977529444ce5392344bf
thanhhungcf268 <thanhhungcf268@gmail.com>
2021-04-09 11:01:35 +0700

    them test Crircle and Cylinder

---
+++ Ke_Thua/CircleTest.java
@@ -0,0 +1,8 @@
+package Ke_Thua;
+
+public class CircleTest {
+    public static void main(String[] args) {
+        Circle circle = new Circle(10,"red");
+        System.out.println(circle);
+    }
+}
---
+++ Ke_Thua/CylinderTest.java
@@ -0,0 +1,8 @@
+package Ke_Thua;
+
+public class CylinderTest {
+    public static void main(String[] args) {
+        Cylinder circle = new Cylinder(10,"red",10);
+        System.out.println(circle);
+    }
+}
